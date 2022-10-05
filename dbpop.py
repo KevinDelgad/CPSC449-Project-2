12a55149378260a9189a726283722fd32e5c42a0
Kevin Delgado <kevindel891@gmail.com>
2022-10-05 15:13:24 -0700

    python script to populate database with valid and correct answers

---
+++ dbpop.py
@@ -0,0 +1,23 @@
+import sqlite3
+import json
+conn = sqlite3.connect('var/wordle.db')
+c = conn.cursor()
+
+
+f = open('share/valid.json')
+data = json.load(f)
+
+for val in data:
+    c.execute("INSERT INTO valid_word (valword) VALUES(?)" , (val,)) 
+f.close()   
+
+
+s = open('share/correct.json')
+data = json.load(s)
+
+for val in data:
+    c.execute("INSERT INTO answer (answord) VALUES(?)" , (val,))
+
+s.close()   
+
+conn.commit()
