2aff481b0b2f198d9a21f68aceb9d4df9d9ba3be
KevinDelgad <kevindel891@gmail.com>
2022-10-08 14:00:47 -0700

    connect database to api

--- wordle.py
+++ wordle.py
@@ -1,9 +1,33 @@
-from quart import Quart, request
+import databases
+import collections
+import dataclasses
+import sqlite3
+import textwrap
+
+import toml
+
+from quart import Quart, g, request, abort
+from quart_schema import QuartSchema, RequestSchemaValidationError, validate_request
 
 app = Quart(__name__)
+QuartSchema(app)
 
-@app.route('/')
-async def hello():
-    return  'hello'
+app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
-app.run() 
+async def _connect_db():
+    database = databases.Database(app.config["DATABASES"]["URL"])
+    await database.connect()
+    return database
+
+
+def _get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = _connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+async def close_connection(exception):
+    db = getattr(g, "_sqlite_db", None)
+    if db is not None:
+        await db.disconnect()
