79cf08388665d979136e8b98815157e996cfe788
KevinDelgad <kevindel891@gmail.com>
2022-10-09 03:17:22 -0700

    create user object and create user command

--- wordle.py
+++ wordle.py
@@ -4,6 +4,7 @@ import dataclasses
 import sqlite3
 import textwrap
 
+import databases
 import toml
 
 from quart import Quart, g, request, abort
@@ -13,6 +14,13 @@ app = Quart(__name__)
 QuartSchema(app)
 
 app.config.from_file(f"./etc/{__name__}.toml", toml.load)
+
+@dataclasses.dataclass
+class User:
+    first_name: str;
+    last_name: str;
+    user_name: str;
+    password: str;
 
 async def _connect_db():
     database = databases.Database(app.config["DATABASES"]["URL"])
@@ -38,3 +46,22 @@ async def test():
     db = await _get_db()
     all_answers = await db.fetch_all("SELECT * FROM answer;")
     return list(map(dict, all_answers))
+
+@app.route("/users/", methods=["POST"])
+@validate_request(User)
+async def create_user(data):
+    db = await _get_db()
+    user = dataclasses.asdict(data)
+    try:
+        id = await db.execute(
+            """
+            INSERT INTO user(fname, lname, username, pssword)
+            VALUES(:first_name, :last_name, :user_name, :password)
+            """,
+            user
+        )
+    except sqlite3.IntegrityError as e:
+        abort(409, e)
+
+    user["id"] = id
+    return user, 201, {"Location": f"/users/{id}"}
