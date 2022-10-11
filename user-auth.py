2001ef144b878d6f5e1cc443d29fee64970ec597
fbecerra07 <90226955+fbecerra07@users.noreply.github.com>
2022-10-10 20:18:06 -0700

    Add appropriate get and connect methods.

--- user-auth.py
+++ user-auth.py
@@ -10,10 +10,6 @@ QuartSchema(app)
 app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
 # Database connection
-dbConn = Database('sqlite+aiosqlite:/var/wordle.db')
-# SQLite needs a cursor to execute commands
-cursor = dbConn.cursor()
-
 async def _connect_db():
     database = databases.Database(app.config["DATABASES"]["URL"])
     await database.connect()
@@ -27,16 +23,16 @@ def _get_db():
 # User authentication endpoint
 @app.route("/user-auth", methods=["GET"])
 async def userAuth( username, password ):
+    db = _get_db()
     # Selection query with placeholders
     # Best practice to avoid SQL injections
     select_query = "SELECT * FROM user WHERE username=%s AND passwrd=%s;"
 
     # Run the command
-    results = cursor.execute( selection_query, ( username, password ) )
+    results = db.execute( selection_query, ( username, password ) )
 
     # Is the user registered?
     if results.fetch_all():
         return { "authenticated": true }, 200
     else:
-        # Return 401 and www-authenticate
         return 401, { "WWW-Authenticate": "Fake Realm" }
