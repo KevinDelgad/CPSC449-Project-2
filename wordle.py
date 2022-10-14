033208a1a112c02ff86616a866d8fcd85913d457
fbecerra07 <90226955+fbecerra07@users.noreply.github.com>
2022-10-13 20:37:50 -0700

    Rewrite user-auth endpoint again.

--- wordle.py
+++ wordle.py
@@ -1,3 +1,5 @@
+from cmath import exp
+from pydoc import doc
 import databases
 import collections
 import dataclasses
@@ -15,12 +17,19 @@ QuartSchema(app)
 
 app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
+
 @dataclasses.dataclass
 class User:
-    first_name: str;
-    last_name: str;
-    user_name: str;
-    password: str;
+    first_name: str
+    last_name: str
+    user_name: str
+    password: str
+
+
+@dataclasses.dataclass
+class Game:
+    username: str
+
 
 async def _connect_db():
     database = databases.Database(app.config["DATABASES"]["URL"])
@@ -41,11 +50,13 @@ async def close_connection(exception):
         await db.disconnect()
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 async def test():
     db = await _get_db()
     all_answers = await db.fetch_all("SELECT * FROM answer;")
     return list(map(dict, all_answers))
+
+
 
 @app.route("/users/", methods=["POST"])
 @validate_request(User)
@@ -53,15 +64,82 @@ async def create_user(data):
     db = await _get_db()
     user = dataclasses.asdict(data)
     try:
+        #Attempt to create new user in database
         id = await db.execute(
             """
             INSERT INTO user(fname, lname, username, passwrd)
             VALUES(:first_name, :last_name, :user_name, :password)
             """,
-            user
+            user,
         )
+    #Return 409 error if username is already in table
     except sqlite3.IntegrityError as e:
         abort(409, e)
 
     user["id"] = id
-    return user, 201, {"Location": f"/users/{id}"}
+    return user, 201
+
+# User authentication endpoint
+@app.route("/user-auth/<string:username>/<string:password>", methods=["GET"])
+async def userAuth( username, password ):
+    db = await _get_db()
+    # Selection query with raw queries
+    select_query = "SELECT * FROM user WHERE username= :username AND passwrd= :password"
+    values = {"username": username, "password": password}
+
+    # Run the command
+    result = await db.fetch_one( select_query, values )
+
+    # Is the user registered?
+    if result:
+        return { "authenticated": "true" }, 200
+    else:
+        return 401, { "WWW-Authenticate": "Fake Realm" }
+
+
+@app.route("/games/", methods=["POST"])
+@validate_request(Game)
+async def create_game(data):
+    db = await _get_db()
+    username = dataclasses.asdict(data)
+    # Check if username is in the database
+    valid_user = await db.fetch_one(
+        "SELECT username FROM user WHERE username = :username", username
+    )
+    if valid_user:
+        # Retrieve User Id from their username
+        userid = await db.fetch_one(
+            "SELECT userid FROM user WHERE username = :username", username
+        )
+
+        # Retrive random ID from the answers table
+        word = await db.fetch_one(
+            "SELECT answerid FROM answer ORDER BY RANDOM() LIMIT 1"
+        )
+        # Check if the retrived word is a repeat for the user, and if so grab a new word
+        while await db.fetch_one(
+            "SELECT answerid FROM games WHERE userid = :userid AND answerid = :answerid",
+            values={"userid": userid[0], "answerid": word[0]},
+        ):
+            word = await db.fetch_one(
+                "SELECT answerid FROM answer ORDER BY RANDOM() LIMIT 1"
+            )
+
+        # Create new game with 0 guesses
+        query = "INSERT INTO game(guesses, gstate) VALUES(:guesses, :gstate)"
+        values = {"guesses": 0, "gstate": "In-progress"}
+        cur = await db.execute(query=query, values=values)
+
+        # Create new row into Games table which connect with the recently connected game
+        query = "INSERT INTO games(userid, answerid, gameid) VALUES(:userid, :answerid, :gameid)"
+        values = {"userid": userid[0], "answerid": word[0], "gameid": cur}
+        cur = await db.execute(query=query, values=values)
+
+        return values, 201
+
+    else:
+        abort(404)
+
+@app.errorhandler(409)
+def conflict(e):
+    return {"error": str(e)}, 409
