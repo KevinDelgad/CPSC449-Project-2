84da36c6e900ed717d9406ca8bbac914f3c10d5e
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-10-14 20:39:44 -0700

    Excluding binary files

--- wordle.py
+++ wordle.py
@@ -159,9 +159,8 @@ async def add_guess(data):
     isAnswer= await db.fetch_one(
         "SELECT * FROM answer as a where (select count(*) from games where gameid = :gameid and answerid = a.answerid)>=1 and a.answord = :word;", currGame
         )
-    print(isAnswer)
     #is guessed word the answer
-    if isAnswer is not None and len(isAnswer) >= 1: #cigar is answer
+    if isAnswer is not None and len(isAnswer) >= 1:
         #update game status
         try:
             id_games = await db.execute(
@@ -172,28 +171,46 @@ async def add_guess(data):
         except sqlite3.IntegrityError as e:
             abort(404, e)
         return currGame,201 #should return correct answer? 
-    #if 1 then word is valid otherwise it isn't valid
+    #if 1 then word is valid otherwise it isn't valid and also check if they exceed guess limit
     isValidGuess = await db.fetch_one("SELECT * from valid_word where valword = :word;", values={"word":currGame["word"]})
-    print(isValidGuess)
-    if(isValidGuess is not None and len(isValidGuess) >= 1):
+    guessNum = await db.fetch_one("SELECT guesses from game where gameid = :gameid",values={"gameid":currGame["gameid"]})
+    if(isValidGuess is not None and len(isValidGuess) >= 1 and guessNum[0] < 6):
         try: 
-            #insert guess word into guess table
-            print("Goes here")
-            id_guess = await db.execute("INSERT INTO guess(gameid,guessedword, accuracy) VALUES(:gameid, :guessedword, :accuracy)",values={"guessedword":currGame["word"],"gameid":currGame["gameid"],"accuracy":"None"})
-            #get current game number of guesses
-            print("id_guess",id_guess)
-            guessNum = await db.fetch_one("SELECT guesses from game where gameid = :gameid",values={"gameid":currGame["gameid"]})
-            print("guessNum",guessNum[0])
+            #make a dict mapping each character and its position from the answer
+            answord = await db.fetch_one("SELECT answord FROM answer as a, games as g  where g.gameid = :gameid and g.answerid = a.answerid",values={"gameid":currGame["gameid"]})
+            ansDict = {}
+            for i in range(len(answord[0])):
+                ansDict[answord[0][i]] = i
+            #compare location of guessed word with answer
+            guess_word = currGame["word"]
+            accuracy = ""
+            for i in range(len(guess_word)):
+                # print(guess_word[i])
+                # print(guess_word[i], guess_word[i] in ansDict)
+                if guess_word[i] in ansDict:
+                    # print(ansDict.get(guess_word[i]))
+                    if ansDict.get(guess_word[i]) == i:
+                        accuracy += u'\u2713'
+                    else:
+                        accuracy += 'O'
+                else:
+                    accuracy += 'X'
+            #insert guess word into guess table with accruracy
+            id_guess = await db.execute("INSERT INTO guess(gameid,guessedword, accuracy) VALUES(:gameid, :guessedword, :accuracy)",values={"guessedword":currGame["word"],"gameid":currGame["gameid"],"accuracy":accuracy})
             #update game table's guess variable by decrementing it
             id_games = await db.execute(
                 """
                 UPDATE game set guesses = :guessNum where gameid = :gameid
                 """,values={"guessNum":(guessNum[0]+1),"gameid":currGame['gameid']}
             )
-            print("id_game",id_games)
             #if after updating game number of guesses reaches max guesses then mark game as finished 
-            if(guessNum[0]+1 == 6):
+            if(guessNum[0]+1 >= 6):
                 #update game status as finished
+                id_games = await db.execute(
+                    """
+                    UPDATE game set gstate = :status where gameid = :gameid
+                    """,values={"status":"Finished","gameid":currGame['gameid']}
+                )
                 return currGame,202
         except sqlite3.IntegrityError as e:
             abort(404, e)
