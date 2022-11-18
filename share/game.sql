5f024b2fbe53e315708ed56955ef3fd19cde11df
karterL <cadamloya@gmail.com>
2022-11-18 15:07:56 -0800

    Removed the search commands in the sql files

--- share/game.sql
+++ share/game.sql
@@ -1,27 +1,32 @@
 PRAGMA foreign_KEYs=ON;
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS guess;
+DROP TABLE IF EXISTS answer;
+DROP TABLE IF EXISTS valid_word;
 
 CREATE TABLE games (
-    gamesid INTEGER PRIMARY KEY,
+    gamesid INTEGER PRIMARY KEY AUTOINCREMENT,
     username INTEGER,
     answerid INTEGER,
-    gameid UUID,
+    gameid TEXT,
     FOREIGN KEY (answerid) REFERENCES answer(answerid),
     FOREIGN KEY(gameid) REFERENCES game(gameid)
 );
 
-CREATE INDEX idx_user_games
-ON games (username, gameid);
+-- CREATE INDEX idx_user_games
+-- ON games (username, gameid);
 
 CREATE TABLE game(
-    gameid UUID PRIMARY KEY,
+    gameid TEXT PRIMARY KEY,
     guesses INTEGER,
     gstate VARCHAR(12)
 );
 
 CREATE TABLE guess(
     guessid INTEGER PRIMARY KEY AUTOINCREMENT,
-    gameid UUID,
+    gameid TEXT,
     guessedword VARCHAR(5),
     accuracy VARCHAR(5),
     FOREIGN KEY(gameid) REFERENCES game(gameid)
@@ -40,40 +45,40 @@ CREATE TABLE valid_word(
     valword VARCHAR(5)
 );
 
-SCAN TABLE answer;
-USE TEMP B-TREE FOR ORDER BY;
+-- SCAN TABLE answer;
+-- USE TEMP B-TREE FOR ORDER BY;
 
 CREATE INDEX games_idx_a114f231 ON games(username, answerid);
 
-SEARCH TABLE games USING COVERING INDEX games_idx_a114f231 (username=? AND answerid=?);
+-- SELECT TABLE games USING COVERING INDEX games_idx_a114f231 (username=? AND answerid=?);
 
 CREATE INDEX games_idx_8df6ac78 ON games(gameid, answerid);
 CREATE INDEX answer_idx_0382b0a6 ON answer(answord);
 
-SEARCH TABLE answer AS a USING COVERING INDEX answer_idx_0382b0a6 (answord=?);
-CORRELATED SCALAR SUBQUERY 1;
-SEARCH TABLE games USING COVERING INDEX games_idx_8df6ac78 (gameid=? AND answerid=?);
+-- SELECT TABLE answer AS a USING COVERING INDEX answer_idx_0382b0a6 (answord=?);
+-- CORRELATED SCALAR SUBQUERY 1;
+-- SELECT TABLE games USING COVERING INDEX games_idx_8df6ac78 (gameid=? AND answerid=?);
 
 CREATE INDEX valid_word_idx_0420916f ON valid_word(valword);
 
-SEARCH TABLE valid_word USING COVERING INDEX valid_word_idx_0420916f (valword=?);
+-- SELECT TABLE valid_word USING COVERING INDEX valid_word_idx_0420916f (valword=?);
 
-SEARCH TABLE game USING INTEGER PRIMARY KEY (rowid=?);
+-- SELECT TABLE game USING INTEGER PRIMARY KEY (rowid=?);
 
-CREATE INDEX games_idx_8df6ac78 ON games(gameid, answerid);
+-- CREATE INDEX games_idx_8df6ac78 ON games(gameid, answerid);
 
-SEARCH TABLE games AS g USING COVERING INDEX games_idx_8df6ac78 (gameid=?);
-SEARCH TABLE answer AS a USING INTEGER PRIMARY KEY (rowid=?);
+-- SELECT TABLE games AS g USING COVERING INDEX games_idx_8df6ac78 (gameid=?);
+-- SELECT TABLE answer AS a USING INTEGER PRIMARY KEY (rowid=?);
 
 CREATE INDEX game_idx_0069bed0 ON game(gstate);
 
-SEARCH TABLE game AS a USING INDEX game_idx_0069bed0 (gstate=? AND rowid=?);
-LIST SUBQUERY 1;
-SEARCH TABLE games USING COVERING INDEX idx_user_games (username=?);
+-- SELECT TABLE game AS a USING INDEX game_idx_0069bed0 (gstate=? AND rowid=?);
+-- LIST SUBQUERY 1;
+-- SELECT TABLE games USING COVERING INDEX idx_user_games (username=?);
 
 CREATE INDEX guess_idx_0067de6f ON guess(gameid);
 
-SEARCH TABLE game AS b USING INTEGER PRIMARY KEY (rowid=?);
-SEARCH TABLE guess AS a USING INDEX guess_idx_0067de6f (gameid=?);
+-- SELECT TABLE game AS b USING INTEGER PRIMARY KEY (rowid=?);
+-- SELECT TABLE guess AS a USING INDEX guess_idx_0067de6f (gameid=?);
 
 COMMIT;
--- share/user.sql
+++ share/user.sql
@@ -1,15 +1,15 @@
 PRAGMA foreign_KEYs=ON;
 BEGIN TRANSACTION;
-CREATE TABLE user( 
-    username VARCHAR(25) PRIMARY KEY NOT NULL, 
+CREATE TABLE user(
+    username VARCHAR(25) PRIMARY KEY NOT NULL,
     passwrd VARCHAR(20) NOT NULL,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL,
     UNIQUE(username)
 );
 
-CREATE INDEX idx_user ON user(userid, username, passwrd);
-SEARCH TABLE user USING COVERING INDEX sqlite_autoindex_user_1 (username=?);
-SEARCH TABLE user USING INDEX sqlite_autoindex_user_1 (username=?);
+CREATE INDEX idx_user ON user(username, passwrd);
+-- SELECT TABLE user USING COVERING INDEX sqlite_autoindex_user_1 (username=?);
+-- SELECT  user USING INDEX sqlite_autoindex_user_1 (username=?);
 
 COMMIT;
